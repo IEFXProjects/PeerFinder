bb99d8e13553b82493e74aadf8d93698d0aa2b42
Nancy Arroyo <nancyarroyo925@gmail.com>
2015-07-28 09:18:37 -0500

    Better looking index

---
+++ searchengine.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+	<title> </title>
+</head>
+<body>
+<center> 
+	<h2> Search Engine</h2>
+<form action= ‘ ./search.php'  method=‘get’>
+	<input type=‘text’ name=‘k’ size=‘50’ />
+	<input type=‘submit’  value=‘search’ />
+</form>
+</center>
+</body>
