ddd757ccce973a6e2173e22350ab38a4dba3108f
Michael Guaglianone <mjgppg@gmail.com>
2015-07-29 09:04:28 -0500

    assorted

--- PhotoUpload.php
+++ PhotoUpload.php
@@ -2,10 +2,11 @@
 require 'functions.php';
 sessionpage();
 retrieveUserInfo();
-$target_dir = "/UserPictureUploads";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_dir = "../UserPictureUploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["image"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+echo $imageFileType;
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -19,27 +20,26 @@ if(isset($_POST["submit"])) {
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    echo "Sorry, a file already exists with that name please try again with a different filename.";
     $uploadOk = 0;
 }
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
+    echo "Sorry, your file is too large.  Try to reduce the quality of the image and upload again";
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
+if($imageFileType != "jpg" or $imageFileType != "png" or $imageFileType != "jpeg" or $imageFileType != "JPG" or $imageFileType != "PNG" or $imageFileType != "JPEG" or $imageFileType != "GIF" or $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    echo " Your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        echo "The file ". basename( $_FILES["fileToUpload"]["image"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
